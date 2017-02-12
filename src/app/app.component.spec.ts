02eb90187f3b4a14ee17fa211c1f706f9720d5b5
jntakpe <j.ntakpe@gmail.com>
2017-02-12 19:49:36 +0100

    Début tp filter

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,8 @@
 /* tslint:disable:no-unused-variable */
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {AppComponent} from './app.component';
-import {FormsModule} from '@angular/forms';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { TaskPipe } from './task.pipe';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
@@ -9,7 +10,8 @@ describe('AppComponent', () => {
     TestBed.configureTestingModule({
       imports: [FormsModule],
       declarations: [
-        AppComponent
+        AppComponent,
+        TaskPipe
       ],
     });
     fixture = TestBed.createComponent(AppComponent);
@@ -25,128 +27,87 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('#header h1').textContent).toContain('Todo APP');
   }));
 
-  it('1) should have 0 remaining tasks', async(() => {
-    const component = fixture.debugElement.componentInstance;
-    expect(component.remainingTasks).toBeDefined('La variable remainingTasks doit être créée');
-  }));
-
-  it('1) should display 3 remaining tasks', async(() => {
+  it('1) should display all task in uppercase', async(() => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('#todo-count strong').textContent).toBe('2 tâches restantes',
-      'Le nombre de tâches restantes (2) doit être affiché dans le span#todo-count');
-  }));
-
-  it('2) should have 3 tasks', async(() => {
+    const labels = compiled.querySelectorAll('.view label');
     const component = fixture.debugElement.componentInstance;
-    expect(component.tasks).toBeDefined('La variable tasks doit être créée');
-    expect(component.tasks.length).toBe(3, 'Le tableau de tâches doit contenir 3 tâches');
+    labels.forEach((label, index) => {
+      expect(label.textContent).toBe(component.tasks[index].label.toUpperCase());
+    });
   }));
 
-  it('2) should display 3 tasks', async(() => {
+  it('2) should display selected style on ALL button by default', async(() => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    const allLink = compiled.querySelector('#filters li:first-child a');
+    expect(allLink.classList.contains('selected')).toBeTruthy('La classe selected devrait être appliquée par défault sur le lien all');
+  }));
+
+  it('2) should display selected style after click on filter link', async(() => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    const allLink = compiled.querySelector('#filters li:first-child a');
+    const activeLink = compiled.querySelector('#filters li:nth-child(2) a');
+    const completedLink = compiled.querySelector('#filters li:last-child a');
+    expect(allLink.classList.contains('selected')).toBeTruthy('La classe selected devrait être appliquée par défault sur le lien all');
+    expect(activeLink.classList.contains('selected')).toBeFalsy('La classe selected devrait être appliquée par défault sur le lien all');
+    expect(completedLink.classList.contains('selected')).toBeFalsy('La classe selected devrait être appliquée par défault sur le lien all');
+    activeLink.click();
+    fixture.detectChanges();
+    expect(allLink.classList.contains('selected')).toBeFalsy('La classe selected devrait être appliquée par défault sur le lien active');
+    expect(activeLink.classList.contains('selected'))
+      .toBeTruthy('La classe selected devrait être appliquée par défault sur le lien active');
+    expect(completedLink.classList.contains('selected'))
+      .toBeFalsy('La classe selected devrait être appliquée par défault sur le lien active');
+    completedLink.click();
+    fixture.detectChanges();
+    expect(allLink.classList.contains('selected')).toBeFalsy('La classe selected devrait être appliquée par défault sur le lien completed');
+    expect(activeLink.classList.contains('selected'))
+      .toBeFalsy('La classe selected devrait être appliquée par défault sur le lien completed');
+    expect(completedLink.classList.contains('selected'))
+      .toBeTruthy('La classe selected devrait être appliquée par défault sur le lien completed');
+    allLink.click();
+    fixture.detectChanges();
+    expect(allLink.classList.contains('selected')).toBeTruthy('La classe selected devrait être appliquée par défault sur le lien all');
+    expect(activeLink.classList.contains('selected')).toBeFalsy('La classe selected devrait être appliquée par défault sur le lien all');
+    expect(completedLink.classList.contains('selected')).toBeFalsy('La classe selected devrait être appliquée par défault sur le lien all');
+  }));
+
+  it('3) should display only active tasks after click on active link', async(() => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     const todoList = compiled.querySelector('#todo-list');
     expect(todoList.children.length).toBe(3, 'La todo list doit afficher les 3 tâches');
-    expect(todoList.querySelector('label[for="task"]').textContent).toBe(fixture.debugElement.componentInstance.tasks[0].label,
-      'Le libelle de la tâche doit être affiché');
+    const activeLink = compiled.querySelector('#filters li:nth-child(2) a');
+    activeLink.click();
+    fixture.detectChanges();
+    expect(todoList.children.length).toBe(2, 'La todo list doit afficher les 2 tâches actives');
   }));
 
-  it('3) should toggle tasks', async(() => {
+  it('3) should display only completed tasks after click on complete link', async(() => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    const initCompleted: boolean = fixture.debugElement.componentInstance.tasks[0].completed;
-    const checkboxTask = compiled.querySelector('#task');
-    checkboxTask.click();
-    fixture.detectChanges();
-    expect(fixture.debugElement.componentInstance.tasks[0].completed).toBe(!initCompleted,
-      'Cocher la checkbox #task doit changer la valeur du champ \'completed\' de la tâche associée');
-  }));
-
-  it('4) should add completed style on task', async(() => {
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    const liElement = compiled.querySelector('#todo-list li');
-    fixture.debugElement.componentInstance.tasks[0].completed = false;
-    fixture.detectChanges();
-    expect(liElement.classList.contains('completed'))
-      .toBeFalsy('L\'élement "#todo-list li" ne devrait pas avoir la classe completed avant d\'être marqué comme fini');
-    fixture.debugElement.componentInstance.tasks[0].completed = true;
-    fixture.detectChanges();
-    expect(liElement.classList.contains('completed'))
-      .toBeTruthy('L\'élement "#todo-list li" devrait avoir la classe completed');
-  }));
-
-  it('5) should call removeTask method', async(() => {
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    const btnDestroy = compiled.querySelector('.destroy');
-    const component = fixture.debugElement.componentInstance;
-    expect(component.removeTask).toBeDefined('La méthode removeTask doit être créée sur le composant');
-    spyOn(component, 'removeTask');
-    btnDestroy.click();
-    expect(component.removeTask).toHaveBeenCalledTimes(1);
-  }));
-
-  it('5) should remove task from list', async(() => {
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    const btnDestroy = compiled.querySelector('.destroy');
     const todoList = compiled.querySelector('#todo-list');
-    const initLength = todoList.children.length;
-    btnDestroy.click();
+    expect(todoList.children.length).toBe(3, 'La todo list doit afficher les 3 tâches');
+    const completedLink = compiled.querySelector('#filters li:last-child a');
+    completedLink.click();
     fixture.detectChanges();
-    expect(todoList.children.length).toBe(initLength - 1, 'Le clic sur le bouton supprimer doit enlever un élément de la liste');
+    expect(todoList.children.length).toBe(1, 'La todo list doit afficher la tâche complète');
   }));
 
-  it('6) should call addTask method', async(() => {
+  it('3) should display all tasks after click on all link', async(() => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    const todoInput = compiled.querySelector('#new-todo');
-    const component = fixture.debugElement.componentInstance;
-    expect(component.addTask).toBeDefined('La méthode addTask doit être créée sur le composant');
-    spyOn(component, 'addTask');
-    todoInput.value = 'Faire des tests';
-    todoInput.dispatchEvent(new KeyboardEvent('keyup', {key: 'enter'}));
-    expect(component.addTask).toHaveBeenCalledTimes(1);
-  }));
-
-  it('6) should add task to list', async(() => {
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    const todoInput = compiled.querySelector('#new-todo');
     const todoList = compiled.querySelector('#todo-list');
-    const initLength = todoList.children.length;
-    todoInput.dispatchEvent(new KeyboardEvent('keyup', {key: 'enter'}));
+    expect(todoList.children.length).toBe(3, 'La todo list doit afficher les 3 tâches');
+    const completedLink = compiled.querySelector('#filters li:last-child a');
+    completedLink.click();
     fixture.detectChanges();
-    expect(todoList.children.length).toBe(initLength + 1, 'La touche entrée doit ajouter un élément à la liste');
-  }));
-
-  it('7) should call toggleAll method', async(() => {
+    const allLink = compiled.querySelector('#filters li:first-child a');
+    allLink.click();
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    const toggleAllInput = compiled.querySelector('#toggle-all');
-    const component = fixture.debugElement.componentInstance;
-    expect(component.toggleAll).toBeDefined('La méthode toggleAll doit être créée sur le composant');
-    spyOn(component, 'toggleAll');
-    toggleAllInput.click();
-    fixture.detectChanges();
-    expect(component.toggleAll).toHaveBeenCalledTimes(1);
-  }));
-
-  it('7) should toggle all tasks', async(() => {
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    const toggleAllInput = compiled.querySelector('#toggle-all');
-    const component = fixture.debugElement.componentInstance;
-    toggleAllInput.click();
-    fixture.detectChanges();
-    expect(component.tasks.filter(task => task.completed).length)
-      .toBe(component.tasks.length, 'Toutes les tâches devraient être complétées suite au premier click sur la checkbox #toggle-all');
-    toggleAllInput.click();
-    fixture.detectChanges();
-    expect(component.tasks.filter(task => task.completed).length)
-      .toBe(0, 'Toutes les tâches devraient être activées suite au deuxième click sur la checkbox #toggle-all');
+    expect(todoList.children.length).toBe(3, 'La todo list doit afficher les 3 tâches');
   }));
 
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-import {AppComponent} from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { TaskPipe } from './task.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskPipe
   ],
   imports: [
     BrowserModule,
