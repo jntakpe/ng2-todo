7297d23bf613b68e100acb36eed4a0d3acec49a8
gauthier peel <gauthier peel>
2017-03-28 18:02:42 +0200

    utilisation d'un vrai enum typescript et d'une classe Task

--- src/app/task.model.ts
+++ src/app/task.model.ts
@@ -1,6 +1,15 @@
-export interface Task {
+export interface TaskInterface {
   label: string;
   completed: boolean;
 }
 
+export class Task implements TaskInterface {
+  constructor(public label: string, public completed: boolean = false) {
+  }
+}
+
 export type Status = 'ALL' | 'COMPLETED' | 'ACTIVE';
+
+export enum TodoCompleteEnum {
+  ALL, ACTIVE, COMPLETED
+}
